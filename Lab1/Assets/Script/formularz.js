21f0bab71d36961a988e96de293a59d5dfb7f8eb
EskaValier <d.soczek1999@gmail.com>
2020-10-19 13:50:01 +0200

    UpgradeFormularz
    dodanie checkbox'a i obsługa go

--- Lab1/Assets/Script/formularz.js
+++ Lab1/Assets/Script/formularz.js
@@ -1,5 +1,7 @@
 document.getElementById("Tytul").style.textAlign="Center";
 
+document.getElementsByTagName("body")[0].style.backgroundSize="100%";
+
 function sprawdz(formularz)
 {
 	for (i = 0; i < formularz.length; i++)
@@ -9,7 +11,11 @@ function sprawdz(formularz)
 		{
 			alert("Proszę wypełnić wszystkie pola!");
 			return false;
-		}
-	}
+        }
+    }
+    if(document.getElementById("regulamin").checked == false){
+        alert("Proszę zaakceptowac regulamin!");
+    }
+    console.log("Poprawne wczytanie danych");
 	return true;
 }
