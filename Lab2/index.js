907a9e583eca2b0e864a7b8de3a984330a7de45f
EskaValier <d.soczek1999@gmail.com>
2020-11-03 11:06:40 +0100

    Update index
    dodanie przycisków oraz skryptu do strony index.html

--- Lab2/index.js
+++ Lab2/index.js
@@ -1,2 +1,34 @@
 let x = document.getElementsByClassName("badge-light");
 x[0].innerHTML = 6; // x[0] to pierwszy znaleziony element
+
+var paragrafy = document.getElementsByTagName("p");
+for (i = 0; i < paragrafy.length; i++){
+    paragrafy[i].style.marginTop="10px";
+    paragrafy[i].style.marginBottom="10px";
+}
+
+$(document).ready(function(){
+    $("#ShowTekst").click(function(){
+      console.log("Text: " + $("#test").text());
+    });
+
+    $("#ShowHTML").click(function(){
+      console.log("HTML: " + $("#test").html());
+    });
+
+    $("#notifications").click(function(){
+        console.log("There will be notification button function");
+    });
+
+    $("#tekst").click(function(){
+        console.log("Welcome " + $("#imie").val());
+    });
+
+    $("#AddTekst").click(function(){
+        $("p").append(" <b>Added text</b>.");
+    });
+      
+    $("#AddListItems").click(function(){
+        $("ol").append("<li>Added item</li>");
+    });
+});
