efc5f08ffae244bd2cae185b90ded5366f846d5f
EskaValier <d.soczek1999@gmail.com>
2020-11-16 20:36:44 +0100

    Update index.html
    poprawa wizualna

--- Lab4/index.js
+++ Lab4/index.js
@@ -1,11 +1,10 @@
-
 var myGamePiece;    //pierwszy element gry - sterowany kwadrat
 var myObstacles = [];   //przeszkody dla obiektu - trzeba omijać je kwadratem
 var myScore;    //wynik jaki uzyskaliśmy
 
 // utworzenie poszczególnych komponentów (gracz i wynik) oraz wystartowanie gry
 function startGame() {
-    myGamePiece = new component(30, 30, "red", 10, 120);
+    myGamePiece = new component(30, 30, "brown", 10, 120);
     myGamePiece.gravity = 0.05;
     myScore = new component("30px", "Consolas", "black", 280, 40, "text");
     myGameArea.start();
