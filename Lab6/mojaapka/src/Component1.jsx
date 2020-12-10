dcf6775694b5622029f919072bca148cee6fd649
EskaValier <d.soczek1999@gmail.com>
2020-12-10 13:53:34 +0100

    Add Lab6

---
+++ Lab6/mojaapka/src/Component1.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Component1 = () => (
+    <li className="countdown">
+        <b>śniadanie</b> - "07:00"
+    </li>
+);
+  
+export default Component1;
---
+++ Lab6/mojaapka/src/Component2.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Component2 = () => (
+    <li className="countdown">
+        <b>obiad</b> - "15:00"
+    </li>
+);
+  
+export default Component2;
