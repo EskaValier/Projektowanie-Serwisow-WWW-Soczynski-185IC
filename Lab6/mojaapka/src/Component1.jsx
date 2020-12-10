c7349224272d07f2378834bcd17396e9c691d91d
EskaValier <d.soczek1999@gmail.com>
2020-12-10 13:52:13 +0100

    Delete Lab6

--- Lab6/mojaapka/src/Component1.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Component1 = () => (
-    <li className="countdown">
-        <b>śniadanie</b> - "07:00"
-    </li>
-);
-  
-export default Component1;
--- Lab6/mojaapka/src/Component2.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Component2 = () => (
-    <li className="countdown">
-        <b>obiad</b> - "15:00"
-    </li>
-);
-  
-export default Component2;
